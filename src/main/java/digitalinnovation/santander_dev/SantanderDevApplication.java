d19af1f6ef2ddcabcedea7bc059901770bc6befe
william-002 <will.i.am.002@hotmail.com>
2025-08-08 15:45:16 -0300

    Initial commit

---
+++ src/main/java/digitalinnovation/santander_dev/SantanderDevApplication.java
@@ -0,0 +1,13 @@
+package digitalinnovation.santander_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/digitalinnovation/santander_dev/SantanderDevApplicationTests.java
@@ -0,0 +1,13 @@
+package digitalinnovation.santander_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
