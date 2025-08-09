bc14e1b51ccba6b3e0d0e1b0669f99b4e06a2530
william-002 <will.i.am.002@hotmail.com>
2025-08-08 21:10:58 -0300

    Implementação camada de domínio

---
+++ src/main/java/digitalinnovation/santander_dev/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package digitalinnovation.santander_dev.domain.repository;
+
+import digitalinnovation.santander_dev.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
